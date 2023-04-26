92ecfd0baeb783b9d93fc3ff6dbc85aacbdc5475
Jim <pnwmakes@gmail.com>
2023-04-25 20:11:26 -0700

    Navigation Part 1

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
 import Main from "./screens/MainComponent";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
-  return <Main />;
+  return (
+    <NavigationContainer>
+      <Main />
+    </NavigationContainer>
+  );
 }
+
 
 
